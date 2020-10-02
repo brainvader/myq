8eaffb1ec6fd1bd79efe0393b236df411d5d6701
brainvader <brainvade@gmail.com>
2020-10-02 21:17:33 +0900

    Add CellForm component

---
+++ components/cell-form.js
@@ -0,0 +1,24 @@
+import { Segment, Label } from 'semantic-ui-react'
+
+import CellMenu from './cell-menu'
+import CellInput from './cell-input'
+import { AddBeforeButton, AddAfterButton } from './cell-button'
+
+export default function CellForm({ name, cellStyle }) {
+    return (
+        <Segment.Group>
+
+            <Segment>
+                <Label attached='top left'>{name}</Label>
+            </Segment>
+
+            <Segment style={cellStyle}>
+                <AddBeforeButton />
+                <CellInput />
+                <CellMenu />
+                <AddAfterButton />
+            </Segment>
+
+        </Segment.Group>
+    )
+}

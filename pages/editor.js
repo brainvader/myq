b571e55355128bc67d0a696ea5f2b5e9d928c787
brainvader <brainvade@gmail.com>
2020-10-02 21:06:21 +0900

    Delete unused code

--- pages/editor.js
+++ pages/editor.js
@@ -1,38 +1,14 @@
-import { Grid, Container, Segment } from 'semantic-ui-react'
-import { Button, Input, Label, Form, TextArea } from 'semantic-ui-react'
+import { Grid, Container } from 'semantic-ui-react'
 
 import TitleInput from '../components/title-input'
 import TagInput from '../components/tag-input'
 import QuestionInput from '../components/question-input'
 import AnswerInput from '../components/answer-input'
 
-const textAreaRows = 3
-
 const cell = {
     position: 'relative',
     paddingTop: '2em'
 }
-
-const cellMenu = {
-    marginTop: `0.5em`,
-    marginBottom: `1em`
-}
-
-const addCellBtn = {
-    position: 'absolute',
-    zIndex: 1,
-}
-
-const addBeforeBtn = {
-    ...addCellBtn,
-    transform: `translate(-50%, -50%)`
-}
-
-const addAfterBtn = {
-    ...addCellBtn,
-    transform: `translate(-50%, 0%)`
-}
-
 
 export default function Editor() {
     return (

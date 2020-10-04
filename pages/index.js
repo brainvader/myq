7c5e2af2f4d7f8cbad21acdade6aeb397746e41a
brainvader <brainvade@gmail.com>
2020-10-04 12:12:30 +0900

    Add quiz list page

--- pages/index.js
+++ pages/index.js
@@ -1,65 +1,73 @@
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import { Checkbox, Grid, Header, Table } from 'semantic-ui-react'
+import { Button, Search, Pagination } from 'semantic-ui-react'
 
 export default function Home() {
   return (
-    <div className={styles.container}>
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+    <div>
+      <Header as='h1' content='Quiz List' textAlign='center' />
 
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
+      <Grid container >
 
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
+        <Grid.Row columns={2}>
 
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
+          <Grid.Column textAlign='left'>
+            <Button.Group>
+              <Button icon='plus' />
+              <Button icon='delete' />
+              <Button icon='sync' />
+            </Button.Group>
+          </Grid.Column>
 
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
+          <Grid.Column textAlign='right'>
+            <Search />
+          </Grid.Column>
 
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
+        </Grid.Row>
 
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
-      </main>
+        <Grid.Row columns={10}>
 
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
-      </footer>
-    </div>
+          <Table celled cllapsing verticalAlign='middle'>
+            <Table.Header>
+              <Table.Row>
+                <Table.HeaderCell />
+                <Table.HeaderCell>Title</Table.HeaderCell>
+                <Table.HeaderCell>Date</Table.HeaderCell>
+                <Table.HeaderCell>Tags</Table.HeaderCell>
+                <Table.HeaderCell />
+              </Table.Row>
+            </Table.Header>
+
+            <Table.Body>
+              <Table.Row>
+                <Table.Cell width={1} textAlign='center'>
+                  <Checkbox />
+                </Table.Cell>
+                <Table.Cell width={2}>Why Quiz?</Table.Cell>
+                <Table.Cell width={1}>2020/10/4</Table.Cell>
+                <Table.Cell width={4}>
+                  JavaScript, Python, Rust, Elm, Learning, Science,
+                </Table.Cell>
+                <Table.Cell width={2} textAlign='center'>
+                  <Button.Group>
+                    <Button icon='edit' />
+                    <Button icon='trash alternate outline' />
+                  </Button.Group>
+                </Table.Cell>
+              </Table.Row>
+            </Table.Body>
+
+            <Table.Footer>
+              <Table.Row textAlign='right'>
+                <Table.HeaderCell colSpan='5'>
+                  <Pagination defaultActivePage={1} totalPages={10} />
+                </Table.HeaderCell>
+              </Table.Row>
+            </Table.Footer>
+          </Table>
+
+        </Grid.Row>
+
+      </Grid>
+    </div >
   )
 }

890617e2403064e042af170f53fee9c56c76e364
brainvader <brainvade@gmail.com>
2020-10-08 15:09:19 +0900

    Update getSchema

--- pages/api/dgraph/schema.js
+++ pages/api/dgraph/schema.js
@@ -24,12 +24,7 @@ const toSchema = (schemaMap) => {
     return schema
 }
 
-const getSchema = async (req, res) => {
-    const client = req.dbClient
-    const initialValue = ''
-    const keys = Object.keys(schemaMap)
-    const reducer = (res, key) => `${res} ${key} ${schemaMap[key]}. \n`
-    const schema = keys.reduce(reducer, initialValue)
+const getSchema = async (_, res) => {
     res.statusCode = 200
     res.setHeader('Content-Type', 'application/json')
     res.end(JSON.stringify(schemaMap))

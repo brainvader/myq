95b6ec7f8d3347341319c4beeedfff71dee2ffa4
brainvader <brainvade@gmail.com>
2020-10-08 14:54:49 +0900

    Update GET api/dgraph/schema

--- pages/api/dgraph/schema.js
+++ pages/api/dgraph/schema.js
@@ -1,7 +1,10 @@
 import * as dgraph from 'dgraph-js-http'
+import nextConnect from 'next-connect';
+import middleware from '../../../middleware/database';
 
-const clientStub = new dgraph.DgraphClientStub(process.env.DGRAPH_URL)
-const dgraphClient = new dgraph.DgraphClient(clientStub)
+const handler = nextConnect();
+
+handler.use(middleware);
 
 const schemaMap = {
     title: 'string',
@@ -13,16 +16,15 @@ const schemaMap = {
     tags: '[string]'
 }
 
-const handler = async (req, res) => {
-    if (req.method === 'GET') {
-        const initialValue = ''
-        const keys = Object.keys(schemaMap)
-        const reducer = (res, key) => `${res} ${key} ${schemaMap[key]}. \n`
-        const schema = keys.reduce(reducer, initialValue)
-        res.statusCode = 200
-        res.setHeader('Content-Type', 'application/json')
-        res.end(JSON.stringify(schemaMap))
-    }
+const getSchema = async (req, res) => {
+    const client = req.dbClient
+    const initialValue = ''
+    const keys = Object.keys(schemaMap)
+    const reducer = (res, key) => `${res} ${key} ${schemaMap[key]}. \n`
+    const schema = keys.reduce(reducer, initialValue)
+    res.statusCode = 200
+    res.setHeader('Content-Type', 'application/json')
+    res.end(JSON.stringify(schemaMap))
 }
 
 async function dropSchema() {
@@ -32,5 +34,7 @@ async function dropSchema() {
 async function setSchema(schema) {
     await dgraphClient.alter({ schema: schema })
 }
+
+handler.get(getSchema)
 
 export default handler;

798b53cc3349f0b5075836d611503ab1ca7c9015
brainvader <brainvade@gmail.com>
2020-10-11 23:03:23 +0900

    Add addQuiz handler

--- pages/api/quizzes/index.js
+++ pages/api/quizzes/index.js
@@ -29,6 +29,36 @@ const getQuizzes = async (req, res) => {
     res.end(JSON.stringify(quizzes))
 }
 
+const addQuiz = async (req, res) => {
+    const createdAt = req.body.createdAt
+    const client = req.dbClient
+    const txn = client.newTxn()
+
+    const quiz = {
+        uid: "_:newQuiz",
+        title: '新規クイズ',
+        date: createdAt,
+        question: [{
+            type: 'text',
+            content: '我思う'
+        }],
+        answer: [{
+            type: 'text',
+            content: '故に我あり'
+        }],
+        tags: ['']
+    }
+
+    const result = await txn.mutate({ setJson: quiz, commitNow: true })
+
+    console.log(`create quiz with uid: ${result.data.uids.newQuiz}`)
+
+    res.statusCode = 200
+    res.setHeader('Content-Type', 'application/json')
+    res.json({ uid: result.data.uids.newQuiz })
+}
+
 handler.get(getQuizzes)
+handler.post(addQuiz)
 
 export default handler;

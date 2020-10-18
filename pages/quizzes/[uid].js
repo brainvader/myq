37fda39427e850d6b87715560a61bf981f8393d1
brainvader <brainvade@gmail.com>
2020-10-18 12:01:26 +0900

    Add useQuiz hook

--- pages/quizzes/[uid].js
+++ pages/quizzes/[uid].js
@@ -1,10 +1,20 @@
 import { useRouter } from 'next/router'
 
+import Editor from '../../components/editor/editor'
+import { useQuiz } from '../../lib/hooks'
+
 const Quiz = () => {
     const router = useRouter()
     const { uid } = router.query
 
-    return <p>Quiz id: {uid}</p>
+    const { data, mutate, isLoading, isError } = useQuiz(uid)
+
+    if (isError) return <div>failed to load</div>
+    if (isLoading) return <div>loading...</div>
+
+    const [quiz] = data.quiz
+
+    return <Editor quiz={quiz} />
 }
 
 export default Quiz

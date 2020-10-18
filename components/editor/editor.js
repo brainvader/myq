82e97e39ea94a98e110e5f830cfad927ba310db3
brainvader <brainvade@gmail.com>
2020-10-18 12:29:01 +0900

    Add Editor component

---
+++ components/editor/editor.js
@@ -0,0 +1,61 @@
+import { useEffect } from 'react'
+import { useRouter } from 'next/router'
+
+import { Grid, Container } from 'semantic-ui-react'
+
+import EditorMenu from '../editor-menu'
+import TitleInput from '../title-input'
+import TagInput from '../tag-input'
+import CellForm from '../cell-form'
+
+import { useQuiz } from '../../lib/hooks'
+
+export default function Editor({ uid }) {
+
+    const router = useRouter()
+
+    useEffect(() => {
+        if (!uid) {
+            router.push('/quizzes')
+        }
+    }, [uid])
+
+    const { data, mutate, isLoading, isError } = useQuiz(uid)
+
+    if (isError) return <div>failed to load</div>
+    if (isLoading) return <div>loading...</div>
+
+    const [quiz] = data.quiz
+
+    return (
+        <Container>
+            <Grid centered>
+                <Grid.Row>
+                    <EditorMenu />
+                </Grid.Row>
+
+                <Grid.Row>
+                    <Grid.Column>
+                        <Grid.Row>
+                            <TitleInput />
+                        </Grid.Row>
+                        <Grid.Row>
+                            <TagInput />
+                        </Grid.Row>
+                    </Grid.Column>
+                </Grid.Row>
+
+
+                <Grid.Row columns={1}>
+                    <Grid.Column textAlign='center'>
+
+                        <CellForm name='Question' />
+                        <CellForm name='Answer' />
+
+                    </Grid.Column>
+                </Grid.Row>
+
+            </Grid >
+        </Container >
+    )
+}
--- pages/quizzes/[uid].js
+++ pages/quizzes/[uid].js
@@ -1,20 +1,12 @@
 import { useRouter } from 'next/router'
 
 import Editor from '../../components/editor/editor'
-import { useQuiz } from '../../lib/hooks'
 
 const Quiz = () => {
     const router = useRouter()
     const { uid } = router.query
 
-    const { data, mutate, isLoading, isError } = useQuiz(uid)
-
-    if (isError) return <div>failed to load</div>
-    if (isLoading) return <div>loading...</div>
-
-    const [quiz] = data.quiz
-
-    return <Editor quiz={quiz} />
+    return <Editor uid={uid} />
 }
 
 export default Quiz

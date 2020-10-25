b01ae93bebdb895e8a93db83ca01e2bb9956bd53
brainvader <brainvade@gmail.com>
2020-10-25 21:09:16 +0900

    Add homeHandler function

--- components/editor-menu.js
+++ components/editor-menu.js
@@ -1,3 +1,4 @@
+import { useRouter } from 'next/router'
 import { mutate } from 'swr'
 
 import { Button } from 'semantic-ui-react'
@@ -5,16 +6,21 @@ import { Button } from 'semantic-ui-react'
 import { requestUpdateQuiz } from '../logics/api'
 
 export default function EditorMenu({ quiz }) {
+    const router = useRouter()
 
     const saveHandler = async () => {
         const newQuiz = await requestUpdateQuiz(quiz)
         mutate(`/api/quizzes/${quiz.uid}`)
     }
 
+    const homeHandler = async () => {
+        router.push('/quizzes')
+    }
+
     return (
         <div>
             <Button.Group>
-                <Button icon='home' />
+                <Button icon='home' onClick={homeHandler} />
                 <Button icon='save' onClick={saveHandler} />
             </Button.Group>
             { '　'}

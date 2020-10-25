5d885dbeb258348bab62e1382744e8703d25cf7e
brainvader <brainvade@gmail.com>
2020-10-25 16:25:56 +0900

    Add detachTag handler

--- pages/api/quizzes/[uid]/tags.js
+++ pages/api/quizzes/[uid]/tags.js
@@ -51,9 +51,35 @@ const addTag = async (req, res) => {
     }
 }
 
-// TODO: handler to delete tag
+const detachTag = async (req, res) => {
+    const { uid } = req.query
+    const { tag } = req.body
+
+    const deleteJson = {
+        uid: uid,
+        tags: [
+            {
+                uid: tag.uid,
+                tag_name: null
+            }
+        ]
+    }
+
+    const client = req.dbClient
+    const txn = client.newTxn()
+
+    try {
+        const untagged = await txn.mutate({ deleteJson: deleteJson, commitNow: true });
+        res.json(untagged.data)
+    } catch (error) {
+        throw error
+    } finally {
+        await txn.discard()
+    }
+}
 
 handler.get(getTags)
 handler.put(addTag)
+handler.delete(detachTag)
 
 export default handler

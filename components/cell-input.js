645035ac6c4e2a5c84f0006bce79fe4a748b5d8b
brainvader <brainvade@gmail.com>
2020-10-28 22:42:54 +0900

    Update CellInput component

--- components/cell-input.js
+++ components/cell-input.js
@@ -1,11 +1,23 @@
+import { useContext } from 'react'
+
 import { Form, TextArea } from 'semantic-ui-react'
+
+import EditorContext from '../components/editor/context'
 
 const textAreaRows = 3
 
-export default function CellInput() {
+export default function CellInput({ cell }) {
+    const { uid } = useContext(EditorContext)
+
+    const inputHandler = (event, data) => {
+        updateTitle(quiz.uid, data.value)
+    }
+
     return (
         <Form>
-            <TextArea rows={textAreaRows} />
+            <TextArea
+                value={cell.content}
+                rows={textAreaRows} />
         </Form>
     )
 }

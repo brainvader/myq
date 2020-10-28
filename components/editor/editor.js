14a16bb93ef3b7619e61b2603cf47908574bf613
brainvader <brainvade@gmail.com>
2020-10-28 22:47:32 +0900

    Update Editor component

--- components/editor/editor.js
+++ components/editor/editor.js
@@ -1,5 +1,3 @@
-import { useEffect } from 'react'
-
 import { Grid, Container } from 'semantic-ui-react'
 
 import EditorMenu from '../editor-menu'
@@ -7,31 +5,23 @@ import TitleInput from '../title-input'
 import TagInput from '../tag-input'
 import CellForm from '../cell-form'
 
-import { useQuiz } from '../../lib/hooks'
 import { requestUpdateQuiz } from '../../logics/api'
 
 export default function Editor() {
-    const { data, mutate, isLoading, isError } = useQuiz(uid)
-
     const autoSave = async () => {
         const newQuiz = await requestUpdateQuiz(data)
         mutate(newQuiz)
     }
 
-    useEffect(() => {
-        // const interval = data ? setInterval(async () => {
-        //     console.log(`pre auto save`, data.tags)
-        //     const newQuiz = await requestUpdateQuiz(data)
-        //     console.log(`post auto save`, newQuiz.tags)
-        //     mutate(newQuiz)
-        // }, 1000) : null
-        // return () => clearInterval(interval)
-    }, [data])
-
-    if (isError) return <div>failed to load</div>
-    if (isLoading) return <div>loading...</div>
-
-    const quiz = data
+    // useEffect(() => {
+    // const interval = data ? setInterval(async () => {
+    //     console.log(`pre auto save`, data.tags)
+    //     const newQuiz = await requestUpdateQuiz(data)
+    //     console.log(`post auto save`, newQuiz.tags)
+    //     mutate(newQuiz)
+    // }, 1000) : null
+    // return () => clearInterval(interval)
+    // }, [data])
 
     return (
         <Container>
@@ -55,12 +45,8 @@ export default function Editor() {
                 <Grid.Row columns={1}>
                     <Grid.Column textAlign='center'>
 
-                        <CellForm name='Question' />
-                        <CellForm name='Answer' />
-                            quiz={quiz} />
-                        <CellForm
-                            name='Answer'
-                            quiz={quiz} />
+                        <CellForm formType='Question' />
+                        <CellForm formType='Answer' />
 
                     </Grid.Column>
                 </Grid.Row>

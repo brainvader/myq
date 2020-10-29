7785c2afbc9ec99928325c6adb261b481c01665a
brainvader <brainvade@gmail.com>
2020-10-29 20:51:43 +0900

    Remove unused code

--- components/cell-menu.js
+++ components/cell-menu.js
@@ -1,20 +1,11 @@
 import { Button } from 'semantic-ui-react'
-import { useState } from 'react'
 
 const cellMenu = {
     marginTop: `0.5em`,
     marginBottom: `1em`
 }
 
-const defaultState = {
-    activeItem: ""
-}
-
 export default function CellMenu() {
-    const [state, setState] = useState(defaultState)
-
-    const { activeItem } = state
-
     const handleButtonClick = (event, data) => console.log(`${JSON.stringify(data)}`)
 
     return (

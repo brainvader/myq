8c26964cfe2f4b5ce540524ea0ff267054b56e9f
brainvader <brainvade@gmail.com>
2020-10-30 10:33:58 +0900

    Add updateCell function

--- components/cell-menu.js
+++ components/cell-menu.js
@@ -1,25 +1,56 @@
+import { useContext } from 'react'
+import { mutate } from 'swr'
+
 import { Button } from 'semantic-ui-react'
+
+import EditorContext from '../components/editor/context'
 
 const cellMenu = {
     marginTop: `0.5em`,
     marginBottom: `1em`
 }
 
-export default function CellMenu() {
+const updateCell = async (cell) => {
+    const body = { cell: cell }
+    const res = await fetch(`/api/cells/${cell.uid}`, {
+        method: 'PUT',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify(body)
+    })
+
+    return res
+}
+
+export default function CellMenu({ cell }) {
+    const { uid } = useContext(EditorContext)
+
+    const setType = async (event, data) => {
+        const newCell = { ...cell, type: data }
+        const response = await updateCell(newCell)
+        if (response.ok) mutate(`/api/quizzes/${uid}`)
+    }
+
     const handleButtonClick = (event, data) => console.log(`${JSON.stringify(data)}`)
 
     return (
         <div style={cellMenu}>
             <Button.Group>
                 <Button
+                    icon='font'
+                    active={cell.type === 'text' ? true : false}
+                    onClick={(e) => setType(e, 'text')} />
+                <Button
                     icon='code'
-                    onClick={handleButtonClick} />
+                    active={cell.type === 'code' ? true : false}
+                    onClick={(e) => setType(e, 'code')} />
                 <Button
                     icon='dollar sign'
-                    onClick={handleButtonClick} />
+                    active={cell.type === 'math' ? true : false}
+                    onClick={(e) => setType(e, 'math')} />
                 <Button
                     icon='linkify'
-                    onClick={handleButtonClick} />
+                    active={cell.type === 'ref' ? true : false}
+                    onClick={(e) => setType(e, 'ref')} />
             </Button.Group>
             {'    '}
             <Button.Group>

7772f2671dacdb24be2ddd134214333a6a488028
brainvader <brainvade@gmail.com>
2020-10-30 20:57:40 +0900

    Update saveQuiz handler

--- pages/api/quizzes/[uid]/index.js
+++ pages/api/quizzes/[uid]/index.js
@@ -41,21 +41,12 @@ const getQuiz = async (req, res) => {
 }
 
 const saveQuiz = async (req, res) => {
-    const quiz = req.body.quiz
-    const client = req.dbClient
-    const txn = client.newTxn();
+    const { quiz } = req.body
+    const txn = req.dbClient.newTxn();
 
     try {
-        const original = await client.newTxn().query(getQuizzQuery(quiz.uid))
-        console.log('original', original.data.quiz[0].tags.join(','))
-
-        const txn = client.newTxn()
         const result = await txn.mutate({ setJson: quiz })
-
-        const post = await client.newTxn().query(getQuizzQuery(quiz.uid))
         await txn.commit();
-        console.log('new', post.data.quiz[0].tags.join(','))
-
         res.statusCode = 200
         res.setHeader('Content-Type', 'application/json')
         res.json(quiz)
@@ -64,7 +55,6 @@ const saveQuiz = async (req, res) => {
     } finally {
         await txn.discard();
     }
-
 }
 
 handler.get(getQuiz)

ae1255cd8456822926af88ce274a6653e82bd7a3
brainvader <brainvade@gmail.com>
2020-11-01 13:21:18 +0900

    Update CellInput component

--- components/cell-input.js
+++ components/cell-input.js
@@ -1,24 +1,42 @@
 import { useContext } from 'react'
+import { mutate } from 'swr'
 
 import { Form, TextArea } from 'semantic-ui-react'
 
 import EditorContext from '../components/editor/context'
-import CellMenu from './cell-menu'
+import CellFormContext from '../components/cell-form/context'
 
 const textAreaRows = 3
 
 export default function CellInput({ cell }) {
     const { uid } = useContext(EditorContext)
+    const { formType } = useContext(CellFormContext)
 
     const inputHandler = (event, data) => {
-        updateTitle(quiz.uid, data.value)
+        const content = data.value
+
+        // update cached quiz without revalidation
+        mutate(`/api/quizzes/${uid}`, async current => {
+            const currentCells = (current[formType] || [])
+            const updatedCells = currentCells.map(currentCell => {
+                // find editing cell
+                return currentCell.uid === cell.uid
+                    // update cell content 
+                    ? { ...currentCell, content: content }
+                    : currentCell
+            })
+            const newQuiz = { ...current, [formType]: [...updatedCells] }
+            console.log(newQuiz)
+            return newQuiz
+        }, false)
     }
 
     return (
         <Form>
             <TextArea
                 value={cell.content}
-                rows={textAreaRows} />
+                rows={textAreaRows}
+                onInput={inputHandler} />
         </Form>
     )
 }

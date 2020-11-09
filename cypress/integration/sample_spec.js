74a2b2ae25c782dbb1e309720ec02abe76da09d7
brainvader <brainvade@gmail.com>
2020-11-10 00:19:58 +0900

    Add response body from stub route

--- cypress/integration/sample_spec.js
+++ cypress/integration/sample_spec.js
@@ -1,3 +1,31 @@
+const timeStamp = '2020-11-09T21:57:34Z'
+
+const newQuiz = {
+    uid: "0x70530",
+    title: "What is Cypress.js",
+    user: "myq",
+    date: timeStamp,
+    question: [
+        {
+            type: 'text',
+            content: 'What is Cypress.js',
+            order: 0
+        }
+    ],
+    answer: [
+        {
+            type: 'text',
+            content: 'E2E testing library',
+            order: 0
+        }
+    ],
+    "tags": [
+        {
+            tag_name: 'cypress'
+        }
+    ]
+}
+
 describe('Home Page', () => {
     it('should render home page', () => {
         cy.server()

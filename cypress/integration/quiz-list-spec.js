d6be87cc5c2e2c8440c7ca664c48f95ab23f9821
brainvader <brainvade@gmail.com>
2020-11-10 21:22:30 +0900

    Update test for quiz list

--- cypress/integration/quiz-list-spec.js
+++ cypress/integration/quiz-list-spec.js
@@ -1,6 +1,16 @@
 describe('Quiz List', () => {
-    it('should get quiz list', () => {
+    it('should show quiz list', () => {
         cy.server()
-        cy.route()
+        // cy.fixture('quizzes').as('quizzes')
+        cy.route('GET', '/api/quizzes?page=1', 'fixture:quizzes').as('getQuizzes')
+        cy.visit("/quizzes")
+        // cy.wait('@getQuizzes').then(console.log)
+
+        //  see https://docs.cypress.io/faq/questions/using-cypress-faq.html#How-do-I-get-an-element%E2%80%99s-text-contents
+        cy.get('tr')
+            .eq(1) // get the first child
+            .children(':nth-child(2)') // get the second td elements
+            .should('contain', 'MinQとは何でしょうか?')
+        // .should('have.text', 'MinQとは何でしょうか?')
     });
 });

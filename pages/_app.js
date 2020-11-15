07e7070c0864292fa044bdf53be15231c870a85c
brainvader <brainvade@gmail.com>
2020-11-15 18:18:15 +0900

    Setup mock server

--- pages/_app.js
+++ pages/_app.js
@@ -3,11 +3,12 @@ import { makeServer } from "../server/mirage"
 import '../styles/globals.css'
 import 'semantic-ui-css/semantic.min.css'
 
+// setup mock server
+const importMock = async () => await import('../mocks')
+
 function MyApp({ Component, pageProps }) {
 
-  if (process.env.NODE_ENV === "development") {
-    makeServer({ environment: "development" })
-  }
+  if (process.env.NODE_ENV !== 'production') importMock()
 
   return <Component {...pageProps} />
 }

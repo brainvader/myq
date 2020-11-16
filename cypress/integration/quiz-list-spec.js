c123cea43270b0c676d4064e10d4118dcfe59479
brainvader <brainvade@gmail.com>
2020-11-16 12:07:15 +0900

    Use testing-library command instead of cypress native command

--- cypress/integration/quiz-list-spec.js
+++ cypress/integration/quiz-list-spec.js
@@ -1,13 +1,6 @@
 describe('Quiz List', () => {
     it('should show quiz list', () => {
         cy.visit("/quizzes")
-
-        // TODO: replace get with findBy*
-        //  see https://docs.cypress.io/faq/questions/using-cypress-faq.html#How-do-I-get-an-element%E2%80%99s-text-contents
-        cy.get('tr')
-            .eq(1) // get the first child
-            .children(':nth-child(2)') // get the second td elements
-            .should('contain', 'MinQとは何でしょうか?')
-        // .should('have.text', 'MinQとは何でしょうか?')
+        cy.findByText('MinQとは何でしょうか?').should('exist')
     });
 });

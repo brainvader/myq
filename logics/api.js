27f1851333876631e34019606f8f9c67caabbd02
brainvader <brainvade@gmail.com>
2020-11-17 21:47:06 +0900

    Extract requests to apis from component files

--- logics/api.js
+++ logics/api.js
@@ -1,4 +1,6 @@
-const requestUpdateQuiz = async (quiz) => {
+import axios from 'axios'
+
+export const requestUpdateQuiz = async (quiz) => {
     const body = { quiz: quiz }
     const res = await fetch(`/api/quizzes/${quiz.uid}`, {
         method: 'PUT',
@@ -8,5 +10,33 @@ const requestUpdateQuiz = async (quiz) => {
     return res
 }
 
+export const requestDeleteCell = async (cell, body) => {
+    const res = await axios.delete(`/api/cells/${cell.uid}`, { data: body })
+    // const res = await fetch(`/api/cells/${cell.uid}`, {
+    //     method: 'DELETE',
+    //     headers: { 'Content-Type': 'application/json' },
+    //     body: JSON.stringify(body)
+    // })
+    return res
+}
 
-export { requestUpdateQuiz }
+export const requestUpdateCellType = async (cell, body) => {
+    // TODO: Update /api/cells/[cellUid]/type
+    const res = await axios.put(`/api/cells/${cell.uid}`, body)
+    // const res = await fetch(`/api/cells/${cell.uid}`, {
+    //     method: 'PUT',
+    //     headers: { 'Content-Type': 'application/json' },
+    //     body: JSON.stringify(body)
+    // })
+    return res
+}
+
+export const requestSwapCells = async (body) => {
+    const res = await axios.put(`/api/cells/orders`, body)
+    // const res = await fetch(`/api/cells/orders`, {
+    //     method: 'PUT',
+    //     headers: { 'Content-Type': 'application/json' },
+    //     body: JSON.stringify(body)
+    // })
+    return res
+}

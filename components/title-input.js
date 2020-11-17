d152e8ea5c226562db71e0516118e50e721dff8e
brainvader <brainvade@gmail.com>
2020-11-17 22:38:25 +0900

    Add requestUpdateTitle function

--- components/title-input.js
+++ components/title-input.js
@@ -3,18 +3,10 @@ import { mutate } from 'swr'
 
 import { Input } from 'semantic-ui-react'
 
+import { requestUpdateTitle, OK } from '../logics/api'
+
 const style = {
     width: '100%'
-}
-
-const updateTitle = async (uid, title) => {
-    const body = { title: title }
-    const res = await fetch(`/api/quizzes/${uid}/title`, {
-        method: 'PUT',
-        headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify(body)
-    })
-    return res
 }
 
 export default function TitleInput({ quiz }) {
@@ -32,8 +24,9 @@ export default function TitleInput({ quiz }) {
 
     const keyboardHandler = async (event) => {
         if (event.key === 'Enter') {
-            const res = await updateTitle(quiz.uid, title)
-            if (res.ok) mutate(`/api/quizzes/${quiz.uid}`)
+            const body = { title: title }
+            const res = await requestUpdateTitle(quiz.uid, body)
+            if (OK(res)) mutate(`/api/quizzes/${quiz.uid}`)
         }
     }
 
